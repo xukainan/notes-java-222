1de79eb7c1fc8e515cf36eb9c55c4afd69bc3d95
xukainan <836127990@qq.com>
2023-04-10 15:27:39 +0800

    leecode

---
+++ notes-aws/src/main/java/top/uaian/aws/NotesAwsApplication.java
@@ -0,0 +1,13 @@
+package top.uaian.aws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotesAwsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NotesAwsApplication.class, args);
+    }
+
+}
